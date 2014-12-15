7b1188e533aa59f8488dd671390617eeae61bce1
Jeremy Symon <jtsymon@gmail.com>
2014-12-15 14:24:10 +1300

    Refactored worker.c
    Moved request handling to another method
    
    Added timeout to request handling (so an idle connection will not lock up a worker)
    
    Added error checking for socket closing because I noticed sockets weren't always being closed properly

--- worker.c
+++ worker.c
@@ -5,6 +5,7 @@
 #include <sys/wait.h>
 #include <netinet/in.h>
 #include <sys/epoll.h>
+#include <errno.h>
 
 #include "cws.h"
 #include "worker.h"
@@ -23,18 +24,110 @@ struct worker {
 static int worker_sock;
 const int worker_count;
 static struct worker *workers = NULL;
+static int worker_id;
 
-static void work (int id, int pipe) {
+static int getSO_ERROR(int fd) {
+    int err = 1;
+    socklen_t len = sizeof err;
+    if (-1 == getsockopt(fd, SOL_SOCKET, SO_ERROR, (char *)&err, &len))
+        fprintf (stderr, "getSO_ERROR\n");
+    if (err)
+        errno = err;              // set errno to the socket SO_ERROR
+    return err;
+}
 
-    setname ("cws[worker]#%d", id);
+static void handle (int fd, struct sockaddr addr, socklen_t addr_len) {
+    if (fd < 0) {
+        return;
+    }
+    fd_set set;
+    struct timeval timeout;
+    FD_ZERO (&set);
+    FD_SET (fd, &set);
+    timeout.tv_sec = 1;
+    timeout.tv_usec = 0;
+    headers_cleanup ();
+    while (select (FD_SETSIZE, &set, NULL, NULL, &timeout) == 1) {
+        char *buffer = malloc(255);
+        int len = read (fd, buffer, 255);
+        if (len <= 0 || headers_consume (len, buffer)) {
+            break;
+        }
+    }
+    if (!headers_has_request()) {
+        goto hangup;
+    }
+    int j;
+    response_init (200);
+    int headers_len = 0;
+    char *headers = get_response(&headers_len);
+    if (!headers || headers_len <= 0 || write (fd, headers, headers_len) < 0) {
+        goto hangup;
+    }
+    char response_buffer[255];
+    sprintf (response_buffer, "Handled by worker #%d\nRequest:\n", worker_id);
+    if (write (fd, response_buffer, strlen (response_buffer)) < 0) {
+        goto hangup;
+    }
+
+    char *field;
+    if ((field = get_request (0))) {
+        sprintf (response_buffer, "Method: %s\n", field);
+        free (field);
+        if (write (fd, response_buffer, strlen (response_buffer)) < 0) {
+            goto hangup;
+        }
+    }
+    if ((field = get_request (1))) {
+        sprintf (response_buffer, "Path: %s\n", field);
+        free (field);
+        if (write (fd, response_buffer, strlen (response_buffer)) < 0) {
+            goto hangup;
+        }
+    }
+    if (headers_has_version () && (field = get_request (2))) {
+        sprintf (response_buffer, "Version: %s\n", field);
+        free (field);
+        if (write (fd, response_buffer, strlen (response_buffer)) < 0) {
+            goto hangup;
+        }
+    }
+
+    sprintf (response_buffer, "Headers:\n");
+    if (write (fd, response_buffer, strlen (response_buffer)) < 0) {
+        goto hangup;
+    }
+    for (j = 0; ; j++) {
+        char *key = get_header (j, 0);
+        char *val = get_header (j, 1);
+        if (!key || !val) {
+            free (key);
+            free (val);
+            break;
+        }
+        if (write (fd, key, strlen(key)) < 0 ||
+                write (fd, " = ", 3) < 0 ||
+                write (fd, val, strlen(val)) < 0 ||
+                write (fd, "\n", 1) < 0) {
+            free (key);
+            free (val);
+            break;
+        }
+        free (key);
+        free (val);
+    }
+hangup:
+    getSO_ERROR(fd);
+    if (close (fd)) {
+        fprintf (stderr, "Failed to close socket\n");
+    }
+}
+
+static void work (int pipe) {
+    setname ("cws[worker]#%d", worker_id);
 
     sighandleall (SIG_IGN, 0);
     sighandle (SIG_DFL, 0, SIGTERM);
-    int clientfd;
-    socklen_t clilen;
-    struct sockaddr_in cli_addr;
-    char *buffer;
-    char response_buffer[255];
 
     non_block (pipe);
 
@@ -45,7 +138,6 @@ static void work (int id, int pipe) {
 
     int i, n;
     while (1) {
-        headers_cleanup ();
         n = epoll_wait (epollfd, events, MAX_QUEUE, -1);
         for (i = 0; i < n; i++) {
             if ((events[i].events & EPOLLERR) ||
@@ -54,76 +146,10 @@ static void work (int id, int pipe) {
                 _Exit(1);
             } else if (worker_sock == events[i].data.fd) {
                 // socket event
-                clilen = sizeof (cli_addr);
-                clientfd = accept (worker_sock, (struct sockaddr *) &cli_addr, &clilen);
-                if (clientfd < 0) {
-                    continue;
-                }
-                int len;
-                do {
-                    buffer = malloc (255);
-                    len = read (clientfd, buffer, 255);
-                    if (headers_consume (len, buffer)) {
-                        break;
-                    }
-                } while (len > 0);
-                
-                int j;
-                response_init (200);
-                sprintf (response_buffer, "%sHandled by worker #%d\nRequest:\n", get_response(), id);
-                if (write (clientfd, response_buffer, strlen (response_buffer)) < 0) {
-                    goto hangup;
-                }
-                char *field;
-                if ((field = get_request (0))) {
-                    sprintf (response_buffer, "Method: %s\n", field);
-                    free (field);
-                    if (write (clientfd, response_buffer, strlen (response_buffer)) < 0) {
-                        goto hangup;
-                    }
-                }
-                if ((field = get_request (1))) {
-                    sprintf (response_buffer, "Path: %s\n", field);
-                    free (field);
-                    if (write (clientfd, response_buffer, strlen (response_buffer)) < 0) {
-                        goto hangup;
-                    }
-                }
-                if (headers_has_version () && (field = get_request (2))) {
-                    if (field) {
-                        sprintf (response_buffer, "Version: %s\n", field);
-                        free (field);
-                        if (write (clientfd, response_buffer, strlen (response_buffer)) < 0) {
-                            goto hangup;
-                        }
-                    }
-                }
-
-                sprintf (response_buffer, "Headers:\n");
-                if (write (clientfd, response_buffer, strlen (response_buffer)) < 0) {
-                    goto hangup;
-                }
-                for (j = 0; ; j++) {
-                    char *key = get_header (j, 0);
-                    char *val = get_header (j, 1);
-                    if (!key || !val) {
-                        free (key);
-                        free (val);
-                        break;
-                    }
-                    if (write (clientfd, key, strlen(key)) < 0 ||
-                            write (clientfd, " = ", 3) < 0 ||
-                            write (clientfd, val, strlen(val)) < 0 ||
-                            write (clientfd, "\n", 1) < 0) {
-                        free (key);
-                        free (val);
-                        goto hangup;
-                    }
-                    free (key);
-                    free (val);
-                }
-                hangup:
-                close (clientfd);
+                struct sockaddr cli_addr;
+                socklen_t cli_len = sizeof (cli_addr);
+                int cli_fd = accept (worker_sock, (struct sockaddr *) &cli_addr, &cli_len);
+                handle (cli_fd, cli_addr, cli_len);
             } else {
                 // pipe event
                 char cmd;
@@ -160,8 +186,9 @@ static void respawn (int id) {
         printf("Child %d is %d\n", id, pid);
     } else {
         // worker
+        worker_id = id;
         close (pipefd[1]);
-        work (id, pipefd[0]);
+        work (pipefd[0]);
         _Exit(0);
     }
 }
